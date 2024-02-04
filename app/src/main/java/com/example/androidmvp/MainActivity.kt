fbb19dacd68c322e70192d4a107eb6f0d64d3ff4
ThanhTan <thanhtan27032001@gmail.com>
2024-02-04 17:27:53 +0700

    [v1.0.0] Login - ui

--- app/src/main/java/com/example/androidmvp/MainActivity.kt
+++ app/src/main/java/com/example/androidmvp/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.androidmvp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -11,9 +14,34 @@ import com.example.androidmvp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var edtEmail: EditText
+    private lateinit var edtPassword: EditText
+    private lateinit var tvMessage: TextView
+    private lateinit var btnLogin: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setView()
+        setEvent()
+
+    }
+
+    private fun login() {
+
+    }
+
+    private fun setEvent() {
+        btnLogin.setOnClickListener {
+            login()
+        }
+    }
+
+    private fun setView() {
+        edtEmail = findViewById(R.id.edtEmail)
+        edtPassword = findViewById(R.id.edtPassword)
+        tvMessage = findViewById(R.id.tvMessage)
+        btnLogin = findViewById(R.id.btnLogin)
     }
 }
