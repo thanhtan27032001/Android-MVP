4a575393cc2d67e661d8fff11d7333133555e239
ThanhTan <thanhtan27032001@gmail.com>
2024-02-04 17:05:34 +0700

    [v1.0.0] General - first commit

---
+++ app/src/main/java/com/example/androidmvp/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.androidmvp
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.androidmvp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
